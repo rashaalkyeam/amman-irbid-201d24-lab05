36f665590c4496e872f14bdb9491ccb626ff29bd
rashaalkyeam <rasha.alqyyam@gmail.com>
2021-04-29 15:05:35 +0300

    splving Problem 3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) {
+function sum(a,b) {
   let sum = a+b;
   return [sum, `The sum of ${a} and ${b} is ${sum}.`];
 }
@@ -26,7 +26,7 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) {
+function multiply(a,b) {
  let product = a*b;
  return [product,`The product of ${a} and ${b} is ${product}.`];
 }
@@ -48,12 +48,22 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
+function sum1(a,b,c) {
+  let sum = a+b+c;
+  return sum;
+}
+function multiply1(a,b,c) {
+  let product = a*b*c;
+  return product;
+ }
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  let sumadd=sum1(a,b,c);
+  let summult=multiply1(a,b,c);
+  return[sumadd,summult,'4 and 7 and 5 sum to 16.','The product of 4 and 7 and 5 is 140.'];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
